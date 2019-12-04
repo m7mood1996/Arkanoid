bfe001e8c471e3a50c75c7466bfa510b35d07302
mahmoodnael <m7mood.nael.1996@gmail.com>
2019-12-04 14:46:51 +0200

    hello

--- app/src/main/java/com/mahmood_anas/arkanoid/GameView.java
+++ app/src/main/java/com/mahmood_anas/arkanoid/GameView.java
@@ -1,4 +1,30 @@
 package com.mahmood_anas.arkanoid;
 
-public class GameView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+
+
+
+
+
+        invalidate();
+    }
 }
