6cf1e5aefa92d395a44821c91d32f897a931a13a
mahmoodnael <m7mood.nael.1996@gmail.com>
2019-12-04 15:48:44 +0200

    hello

--- app/src/main/java/com/mahmood_anas/arkanoid/Brick.java
+++ app/src/main/java/com/mahmood_anas/arkanoid/Brick.java
@@ -1,4 +1,70 @@
 package com.mahmood_anas.arkanoid;
 
+import android.graphics.Paint;
+
 public class Brick {
+
+    private float x;
+    private float y;
+    private float right;
+    private float bottom;
+    private boolean visible;
+    private Paint p;
+
+    public Brick(float x, float y, float right, float bottom,Paint p) {
+        this.x = x;
+        this.y = y;
+        this.right = right;
+        this.bottom = bottom;
+        this.visible = true;
+        this.p = p;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getRight() {
+        return right;
+    }
+
+    public void setRight(float right) {
+        this.right = right;
+    }
+
+    public float getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(float bottom) {
+        this.bottom = bottom;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public Paint getP() {
+        return p;
+    }
+
+    public void setP(Paint p) {
+        this.p = p;
+    }
 }
--- app/src/main/java/com/mahmood_anas/arkanoid/GameView.java
+++ app/src/main/java/com/mahmood_anas/arkanoid/GameView.java
@@ -10,21 +10,53 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private String lives;
+    private String score;
+    int lives_left;
+    int score_ern;
+    private Brick brick;
+    private int canvasWidth ;
+    private int canvasHeight;
+    private final int ROWS =4;
+    private final int COLs = 10;
+    private Paint textPaint;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        score_ern =0;
+        lives_left =3;
+        lives = "Lives: " + lives_left;
+        score = "Score: " + score_ern;
+        Paint p =new Paint();
+        p.setColor(Color.BLUE);
+        p.setStyle(Paint.Style.FILL);
+        textPaint = new Paint();
+        textPaint.setStyle(Paint.Style.FILL);
+        int c = Color.rgb(255, 195, 0);
+        textPaint.setColor(c);
+        textPaint.setTextSize(50);
+        brick = new Brick(10,10,100,50,p);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-
+        canvas.drawRGB(144, 12, 63);
+        canvas.drawText(score,canvasHeight/25,canvasWidth/25,textPaint);
+        canvas.drawRect(brick.getX(),brick.getY(),brick.getRight(),brick.getBottom(),brick.getP());
 
 
 
 
 
         invalidate();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasWidth = w;
+        canvasHeight = h;
     }
 }
